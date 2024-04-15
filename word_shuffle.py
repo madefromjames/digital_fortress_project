0ddf2e67e530823ed69f451ac1ab7356111b8b6c
Gracehills <azubuikechinedu55@gmail.com>
2024-04-15 15:05:58 +0100

    comment

--- word_shuffle.py
+++ word_shuffle.py
@@ -69,6 +69,8 @@ class WordShuffle:
         # Function to handle when the Submit button is clicked
         pass  # Placeholder for submitting the word
 
+    # Lets work on a new game with no classes
+
 def main():
     # Create the tkinter root window and initialize the WordShuffle game
     win = tk.Tk()
